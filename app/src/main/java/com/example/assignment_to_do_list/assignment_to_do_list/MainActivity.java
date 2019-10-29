147ec63be87207c8116c18de8159937e51b19f39
Jawad Adil <51061044+adiljay05@users.noreply.github.com>
2019-10-29 15:44:20 +0500

    This is the initial version which only contains main activity.

---
+++ app/src/main/java/com/example/assignment_to_do_list/assignment_to_do_list/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.assignment_to_do_list.assignment_to_do_list;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
