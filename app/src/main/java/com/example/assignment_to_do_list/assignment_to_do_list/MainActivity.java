e0482f9e52acfab0c550972259d9639226ddd3fb
Hafiz Waqas Adil <hafizwaqas.adil@student.griffith.ie>
2019-11-12 20:58:07 +0000

    1. Layout changes 2. Date fix for bracket 7 3. Font size fixed

--- app/src/main/java/com/example/assignment_to_do_list/assignment_to_do_list/MainActivity.java
+++ app/src/main/java/com/example/assignment_to_do_list/assignment_to_do_list/MainActivity.java
@@ -37,6 +37,26 @@ public class MainActivity extends Activity {
     CustomAdapter ad;
     task taskObject=new task();
 
+    public String calculateYear(String strDate)
+    {
+        return String.valueOf(strDate.charAt(strDate.length()-4))+String.valueOf(strDate.charAt(strDate.length()-3))+
+                String.valueOf(strDate.charAt(strDate.length()-2))+String.valueOf(strDate.charAt(strDate.length()-1));
+    }
+    public String calculateMonth(String strDate)
+    {
+        return String.valueOf(strDate.charAt(strDate.length()-6))+String.valueOf(strDate.charAt(strDate.length()-5));
+    }
+    public String calculateDay(String strDate)
+    {
+        if(strDate.length()==8)
+        {
+            return String.valueOf(strDate.charAt(strDate.length()-8))+ String.valueOf(strDate.charAt(strDate.length()-7));
+        }
+        else {
+            return String.valueOf(strDate.charAt(strDate.length()-7));
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         overridePendingTransition(0, 0);
@@ -98,8 +118,12 @@ public class MainActivity extends Activity {
                     taskObject.lowestName="*Nill*";
                 }
                 data obj=new data();
-                obj.name=data.getString(1);
-                obj.itemName=taskObject.description+"\t\t\t\t\t* Past Item: "+taskObject.lowestName;
+                obj.name="List Name: "+data.getString(1);
+                String year1="",month1="",day10="";
+                year1=calculateYear(taskObject.dueDate);
+                month1=calculateMonth(taskObject.dueDate);
+                day10=calculateDay(taskObject.dueDate);
+                obj.itemName="Item: "+taskObject.description+"  Date: "+day10+"/"+month1+"/"+year1+"\t\t* Past Item: "+taskObject.lowestName;
                 obj.time=taskObject.dueDate;
                 obj.lowestTimeEver=taskObject.lowestTime;
                 array1.add(obj);
